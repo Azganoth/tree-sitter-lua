29a5230e9c5cad269717cbd6ca214434decc8fae
Ademir José Ferreira Júnior <ademirj.ferreirajunior@gmail.com>
2022-05-20 18:19:36 -0300

    Convert scanner to C

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -11,15 +11,15 @@ fn main() {
     c_config.file(&parser_path);
 
     // If using an external scanner written in C, include this block of code:
-    /* let scanner_path = src_dir.join("scanner.c");
+    let scanner_path = src_dir.join("scanner.c");
     c_config.file(&scanner_path);
-    println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap()); */
+    println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
 
     c_config.compile("parser");
     println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
 
     // If using an external scanner written in C++, include this block of code:
-    let mut cpp_config = cc::Build::new();
+    /* let mut cpp_config = cc::Build::new();
     cpp_config.cpp(true);
     cpp_config.include(&src_dir);
     cpp_config
@@ -28,5 +28,5 @@ fn main() {
     let scanner_path = src_dir.join("scanner.cc");
     cpp_config.file(&scanner_path);
     cpp_config.compile("scanner");
-    println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
+    println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap()); */
 }
